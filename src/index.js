ddaf0b3579a2ac98234e95479602b30d90a4d338
Victor Bussiki <victorbussiki@gmail.com>
2023-03-03 15:05:21 -0300

    Configuração da Base de Dados

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://0.0.0.0/villainbnb');
+//Passar URL da Base de Dados em 'mongoose.connect(_)'
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
 
 app.listen(3000, () => console.log('Server Started'));
