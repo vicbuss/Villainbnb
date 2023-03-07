c36ab15a47b6482064b36e7bed9037b354731adf
Victor Bussiki <victorbussiki@gmail.com>
2023-03-07 14:40:35 -0300

    Função média de temperaturas

--- weatherTest.js
+++ weatherTest.js
@@ -1,19 +1,28 @@
 const axios = require('axios');
+const add = require('date-fns/add');
+const format = require('date-fns/format');
 require('dotenv').config();
 
-async function getWeatherHistory (cidade, data) {
+async function getWeatherHistory (cidade) {
     const key = process.env.WEATHER_API;
-    const url = `http://api.weatherapi.com/v1/history.json?key=${key}&q=${cidade}&dt=${data}`;
-    try {
-        const response = await axios.get(url);
-        const mediaDoDia = response.data.forecast.forecastday[0].day.avgtemp_c;
-        console.log(mediaDoDia);
-    } catch (erro) {
-        console.log(erro.message);
+    let mediasDeTemperatura = [];
+    for(let i = -1; i > -8; i--) {
+        //Começa pela data de ontem, que é formatada em yyyy-mm-dd. A cada laço subtrai-se um dia
+        let data = format(add(new Date(), {days: i}), 'yyyy-LL-dd')
+        let url = `http://api.weatherapi.com/v1/history.json?key=${key}&q=${cidade}&dt=${data}`;
+        try {
+            const response = await axios.get(url);
+            const mediaDoDia = response.data.forecast.forecastday[0].day.avgtemp_c;
+            mediasDeTemperatura.push(mediaDoDia);
+        } catch (erro) {
+            console.log(erro.message);
+        }
     }
+    let mediaDaSemana = mediasDeTemperatura.reduce((a , b) => a + b, 0) / mediasDeTemperatura.length;
+    console.log(mediaDaSemana);
 }
 
 const cidade = 'New York';
-const data = '2023-03-06';
 
-getWeatherHistory(cidade, data);
+getWeatherHistory(cidade);
+
